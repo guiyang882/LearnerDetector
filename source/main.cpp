a8f1d929fdfeed3e0d29ae05d32404d90f8e0811
liuguiyangnwpu <liuguiyangnwpu@163.com>
2016-12-08 11:00:28 +0800

    开始调整框架进行标准数据的测试

--- source/main.cpp
+++ source/main.cpp
@@ -14,66 +14,63 @@
 using namespace std;
 using namespace cv;
 
-//void testOnTLDDataset() {
-//    string dir("/Users/Orthocenter/Developments/TLD/dataset2/06_car/");
-//
-//    string initFilename(dir + "init.txt");
-//    string retFilename(dir + "myRet7.txt");
-//
-//    FILE *fin = fopen(initFilename.c_str(), "r");
-//    FILE *fout = fopen(retFilename.c_str(), "w");
-//
-//    VideoController videoController(dir, ".jpg");
-//    ViewController viewController(&videoController);
-//
-//    videoController.readNextFrame();
-//
-//    int tlx, tly, brx, bry;
-//    fscanf(fin, "%d,%d,%d,%d", &tlx, &tly, &brx, &bry);
-//    fprintf(fout, "%d,%d,%d,%d\n", tlx, tly, brx, bry);
-//
-//    Rect rect = Rect(Point2d(tlx, tly), Point2d(brx, bry));
-//    cout << "Input Rect : " <<  rect << endl;
-//
-//    viewController.refreshCache();
-//    viewController.drawRect(rect, COLOR_BLUE);
-//    viewController.showCache();
-//    waitKey();
-//
-//    TLD tld(videoController.getCurrFrame(), rect);
-//
-//    while(videoController.readNextFrame()) {
-//        cout << "Frame #" << videoController.frameNumber() << endl;
-//        tld.setNextFrame(videoController.getCurrFrame());
-//
-//        Rect bbTrack;
-//        TYPE_DETECTOR_RET bbDetect;
-//
-//        clock_t st = clock();
-//
-//        tld.track();
-//
-//        clock_t ed = clock();
-//        cout << "Time : " << (double)(ed - st) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
-//
-//        viewController.refreshCache();
-//        viewController.drawRect(tld.getBB(), COLOR_GREEN, 2);
-//        viewController.showCache();
-//
-//        cout << endl;
-//
-//        Rect retBB = tld.getBB();
-//        if(retBB == Rect(Point2d(-1, -1), Point2d(-1, -1))) {
-//            fprintf(fout, "NaN,NaN,NaN,NaN\n");
-//        } else {
-//            fprintf(fout, "%d,%d,%d,%d\n", retBB.tl().x, retBB.tl().y, retBB.br().x, retBB.br().y);
-//        }
-//    }
-//
-//    fclose(fin);
-//    fclose(fout);
-//}
-//
+void testOnTLDDataset() {
+    string dir("/Users/liuguiyang/Documents/CodeProj/ConsoleProj/TLD/data/Car24/");
+
+    string initFilename(dir + "init.txt");
+    string retFilename(dir + "myRet7.txt");
+
+    FILE *fin = fopen(initFilename.c_str(), "r");
+    FILE *fout = fopen(retFilename.c_str(), "w");
+
+    VideoController videoController(dir, ".jpg");
+
+    videoController.readNextFrame();
+
+    int tlx, tly, brx, bry;
+    fscanf(fin, "%d,%d,%d,%d", &tlx, &tly, &brx, &bry);
+    fprintf(fout, "%d,%d,%d,%d\n", tlx, tly, brx, bry);
+
+    Rect rect = Rect(Point2d(tlx, tly), Point2d(brx, bry));
+    cout << "Input Rect : " <<  rect << endl;
+
+    videoController.drawRect(rect, COLOR_BLUE);
+    videoController.showCache();
+    waitKey();
+
+    TLD tld(videoController.getCurrFrame(), rect);
+
+    while(videoController.readNextFrame()) {
+        cout << "Frame #" << videoController.frameNumber() << endl;
+        tld.setNextFrame(videoController.getCurrFrame());
+
+        Rect bbTrack;
+        TYPE_DETECTOR_RET bbDetect;
+
+        clock_t st = clock();
+
+        tld.track();
+
+        clock_t ed = clock();
+        cout << "Time : " << (double)(ed - st) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
+
+        videoController.drawRect(tld.getBB(), COLOR_GREEN, 2);
+        videoController.showCache();
+
+        cout << endl;
+
+        Rect retBB = tld.getBB();
+        if(retBB == Rect(Point2d(-1, -1), Point2d(-1, -1))) {
+            fprintf(fout, "NaN,NaN,NaN,NaN\n");
+        } else {
+            fprintf(fout, "%d,%d,%d,%d\n", retBB.tl().x, retBB.tl().y, retBB.br().x, retBB.br().y);
+        }
+    }
+
+    fclose(fin);
+    fclose(fout);
+}
+
 //void testOnVideo() {
 //    string filename("/Users/Orthocenter/Developments/TLD/1.m4v");
 //
@@ -215,9 +212,9 @@ void testOnCamera() {
 }
 
 int main(int argc, char *argv[]) {
-    //testOnTLDDataset();
-    //testOnVideo();
-    testOnCamera();
+    testOnTLDDataset();
+//    testOnVideo();
+//    testOnCamera();
     
     //stabilize();
     return 0;
